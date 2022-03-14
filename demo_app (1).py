c885157f1a86a46a65c9716f8ee94f9fcb16258e
Harish Guragol <42379451+HarishGuragol@users.noreply.github.com>
2022-03-14 20:09:46 +0530

    first commit

---
+++ demo_app (1).py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+import json
+
+app=Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route(f'/fetch', methods=['GET', 'POST'])
+def fetch_data():
+    with open('data.json', 'r') as f:
+        data = f.read()
+    return json.dumps(data), 200, {'ContentType':'application/json'}
+
+@app.route('/store', methods=['POST'])
+def store_data():
+    data = request.get_json()
+    with open('data.json', 'w') as f:
+        f.write(json.dumps(data))
+
+    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+
+if __name__=="__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
